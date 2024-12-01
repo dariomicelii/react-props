b8c31441c015a2f2059d7ea0e3697d1f9783431c
dariomicelii <dariomiceli01@gmail.com>
2024-12-01 17:53:13 +0100

    Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,21 +1,26 @@
-export default function Card() {
+import "../data/posts";
+import Button from "./Button";
+import Title from "./Title";
+
+export default function Card({
+  id,
+  title,
+  image,
+  description,
+  tags,
+  published,
+}) {
   return (
     <div className="card my-5">
       <div className="img-content">
-        <img
-          src="https://i.ytimg.com/vi/kZ0NosptZd8/maxresdefault.jpg"
-          className="img-fluid card-img-top"
-          alt=""
-        />
+        <img src={image} className="img-fluid card-img-top" alt="" />
       </div>
       <div className="text-content">
         <div className="container p-2">
-          <h5>Title</h5>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
-            reprehenderit aperiam repellat impedit!
-          </p>
-          <div className="btn btn-primary">Leggi di più</div>
+          <h5>{title}</h5>
+          <p>{description}</p>
+          <Button text="LEGGI DI PIU'" />
+          <p>{tags}</p>
         </div>
       </div>
     </div>
