3d283603cff25cef9d0657946f8d6d1bd4263243
dariomicelii <dariomiceli01@gmail.com>
2024-12-01 17:53:21 +0100

    Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+export default function () {
+  return (
+    <footer>
+      <div className="container text-center">
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit
+          perferendis soluta quam cum autem, nobis fuga, distinctio reiciendis
+          modi, hic explicabo perspiciatis accusamus quod nihil enim non numquam
+          dolor atque!
+        </p>
+      </div>
+    </footer>
+  );
+}
