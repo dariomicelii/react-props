034a6f64ec1b0c1ee6c9eeebcdd7aea4b493764a
dariomicelii <dariomiceli01@gmail.com>
2024-12-01 17:52:48 +0100

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,20 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+
 function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  return (
+    <div className="wrapper">
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
