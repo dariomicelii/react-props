4e75c4a2bdf80c3d24adaa4acf4e3be68c0dac41
dariomicelii <dariomiceli01@gmail.com>
2024-12-02 10:48:30 +0100

    Tags.jsx

--- src/components/Tags.jsx
+++ src/components/Tags.jsx
@@ -3,7 +3,7 @@ import { posts } from "../data/posts";
 export default function tagslist(tag) {
   return (
     <ul className="tag-list">
-      <li>{tag}</li>
+      <li>{tags}</li>
     </ul>
   );
 }
